85a38d40fe01154c6b2e57bfc3f876e16101b61e
Azure1609 <holuong1609@gmail.com>
2016-07-01 17:04:25 +0700

    Google Maps: Move camera to my location

--- app/src/main/java/fragments/MapsFragment.java
+++ app/src/main/java/fragments/MapsFragment.java
@@ -1,6 +1,11 @@
 package fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +24,38 @@ import com.motthoidecode.findplacesnearby.R;
  */
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
+    private static final float DEFAULT_ZOOM = 15;
+    private static final int TIME_OUT = 40;
+
     private static GoogleMap mMap;
+
+    private Location mCurrentLocation;
+
+    private Handler mHandler = new Handler();
+    private int count = 0;
+    private Runnable mGetMyLocation = new Runnable() {
+        @Override
+        public void run() {
+            count++;
+            if (mMap == null)
+                return;
+            mCurrentLocation = mMap.getMyLocation();
+            if (mCurrentLocation == null && count < TIME_OUT) {
+                mHandler.postDelayed(this, 100);
+            } else {
+                if (mCurrentLocation == null) {
+                    // Add a marker in Sydney and move the camera
+                    LatLng sydney = new LatLng(-34, 151);
+                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                } else {
+                    // Add a marker in current Location and move the captureImage
+                    LatLng mLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, DEFAULT_ZOOM));
+                }
+            }
+        }
+    };
 
     public MapsFragment() {
     }
@@ -41,10 +77,14 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        setUpMap();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void setUpMap() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mHandler.postDelayed(mGetMyLocation, 100);
     }
 }
